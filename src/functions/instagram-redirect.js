809e46f85f2c28592934d3d42a286755b73907cb
jakezien <jakezien@hey.com>
2021-10-22 18:31:55 -0400

    trying instagram-redirect

---
+++ src/functions/instagram-redirect.js
@@ -0,0 +1,75 @@
+// Modules imports
+const cookie = require('cookie');
+const crypto = require('crypto');
+
+// Instagram scopes requested.
+const OAUTH_SCOPES = 'basic';
+
+// Path to the OAuth handlers.
+const OAUTH_CALLBACK_PATH = '/instagram-callback';
+
+// Instagram OAuth 2 setup
+const credentials = {
+  client: {
+    id: process.env.CLIENT_ID,
+    secret: process.env.CLIENT_SECRET
+  },
+  auth: {
+    tokenHost: 'https://api.instagram.com',
+    tokenPath: '/oauth/access_token'
+  }
+};
+const oauth2 = require('simple-oauth2').create(credentials);
+
+
+exports.handler = async function (event, context, callback) {
+
+  const state = event.headers.cookies.state || crypto.randomBytes(20).toString('hex');
+  console.log('Setting state cookie for verification:', state);
+  const secureCookie = event.path.indexOf('localhost:') !== 0;
+  console.log('Need a secure cookie (i.e. not on localhost)?', secureCookie);
+  
+  const serializedCookie = cookie.serialize(
+    'state',
+    state,
+    {
+      secure: secureCookie,
+      httpOnly: true,
+      maxAge: 3600000
+    }
+  )
+  
+  const redirectUri = oauth2.authorizationCode.authorizeURL({
+    redirect_uri: `https://jakestagram.com/${OAUTH_CALLBACK_PATH}`,
+    scope: OAUTH_SCOPES,
+    state: state
+  })
+
+  console.log('Redirecting to:', redirectUri);
+
+  
+  return {
+    'statusCode': 302,
+    'headers': {
+      'Set-Cookie': serializedCookie,
+      'Cache-Control': 'no-cache',
+      'Location': redirectUri
+    },
+    'body': 'redirect'
+  }
+  
+  // app.get(OAUTH_REDIRECT_PATH, (req, res) => {
+  //   const state = req.cookies.state || crypto.randomBytes(20).toString('hex');
+  //   console.log('Setting state cookie for verification:', state);
+  //   const secureCookie = req.get('host').indexOf('localhost:') !== 0;
+  //   console.log('Need a secure cookie (i.e. not on localhost)?', secureCookie);
+  //   res.cookie('state', state, { maxAge: 3600000, secure: secureCookie, httpOnly: true });
+  //   const redirectUri = oauth2.authorizationCode.authorizeURL({
+  //     redirect_uri: `${req.protocol}://${req.get('host')}${OAUTH_CALLBACK_PATH}`,
+  //     scope: OAUTH_SCOPES,
+  //     state: state
+  //   });
+  //   console.log('Redirecting to:', redirectUri);
+  //   res.redirect(redirectUri);
+  // });
+}
