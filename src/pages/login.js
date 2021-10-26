ade90ec70ad8ca5f6753fa8572f90b7a84956f8d
jakezien <jakezien@hey.com>
2021-10-26 17:53:01 -0400

    update login page styling

--- src/pages/login.js
+++ src/pages/login.js
@@ -36,7 +36,7 @@ export default function Login() {
     event.preventDefault();
 
     var actionCodeSettings = {
-      url: 'http://localhost:3000' + ROUTES.LOGIN,
+      url: 'https://jakestagram.com' + ROUTES.LOGIN,
       // This must be true.
       handleCodeInApp: true
     };
@@ -45,12 +45,16 @@ export default function Login() {
       await firebase.auth().sendSignInLinkToEmail(emailAddress, actionCodeSettings)
       window.localStorage.setItem('emailForSignIn', emailAddress);
       // history.push(ROUTES.FEED);
+      setError('')
+      setSuccess('Check your email!\nWe sent you a magic signin link.')
     } catch (error) {
       setEmailAddress("");
       setPassword("");
       setError(error.message);
     }
   }
+
+
 
   const handleSignInWithLink = async (event) => {
     // Confirm the link is a sign-in with email link.
@@ -95,48 +99,48 @@ export default function Login() {
 
   const onSignInWithInstagramButtonClick = () => {
     // Open the Auth flow in a popup.
-    window.open('/.netlify/functions/instagram-redirect', 'Sign In With Instagram', 'height=315,width=400');
+    window.open('/.netlify/functions/instagram-redirect', 'Sign In With Instagram', 'height=315,width=800');
   };
 
-  const fetchData = async() => {
-    const results = await axios.get('/.netlify/functions/auth')
-    console.log(results)
-  }
+  // const fetchData = async() => {
+  //   const results = await axios.get('/.netlify/functions/auth')
+  //   console.log(results)
+  // }
 
-  const fetchInstagramToken = async (code) => {
-    const result = await axios({
-      method: 'post',
-      url: '/.netlify/functions/auth',
-      data: {
-        code: code
-      },
-      // withCredentials: true
-    })
-    return result
-  }
+  // const fetchInstagramToken = async (code) => {
+  //   const result = await axios({
+  //     method: 'post',
+  //     url: '/.netlify/functions/auth',
+  //     data: {
+  //       code: code
+  //     },
+  //     // withCredentials: true
+  //   })
+  //   return result
+  // }
 
-  const fetchInstagramUsername = async (code) => {
-    const result = await axios({
-      method: 'get',
-      url: 'https://graph.instagram.com/me',
-      params: {
-        'fields': 'id,username',
-      },
-      withCredentials: true
-    })
-    console.log('fetchInstagramToken', result)
-    return result
-  }
+  // const fetchInstagramUsername = async (code) => {
+  //   const result = await axios({
+  //     method: 'get',
+  //     url: 'https://graph.instagram.com/me',
+  //     params: {
+  //       'fields': 'id,username',
+  //     },
+  //     withCredentials: true
+  //   })
+  //   console.log('fetchInstagramToken', result)
+  //   return result
+  // }
 
-  const handleInstagramResponse = async (response) => {
-    let result = await fetchInstagramToken(response)
-    let username = await fetchInstagramUsername()
-    // console.log(response)
-    console.log(result)
-  }
+  // const handleInstagramResponse = async (response) => {
+  //   let result = await fetchInstagramToken(response)
+  //   let username = await fetchInstagramUsername()
+  //   // console.log(response)
+  //   console.log(result)
+  // }
  
   useEffect(() => {
-    document.title = "Login - Jakestagram";
+    document.title = "Sign In to Jakestagram";
     handleSignInWithLink()
     // fetchData()
   }, []);
@@ -144,61 +148,58 @@ export default function Login() {
 
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <div className="flex w-3/5">
+      {/* <div className="flex w-3/5">
         <img
           src=""
           alt=""
         />
-      </div>
-      <div className="flex flex-col w-2/5">
-        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
-          <h1 className="flex justify-center w-full">
-            <img
-              src="/images/logo.svg"
-              alt="Jakestagram"
-              className="mt-2 w-6/12 mb-4"
-            />
-          </h1>
+      </div> */}
+      <div className="w-full ">
+        <div className="flex flex-col w-2/5 mx-auto">
+          <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+            <h1 className="flex justify-center w-full">
+              <img
+                src="/images/logo.svg"
+                alt="Jakestagram"
+                className="mt-2 w-6/12 mb-4"
+              />
+            </h1>
 
-          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
-          {success && <p className="mb-4 text-xs text-green-primary">{success}</p>}
-          <p className="text-center text-sm mb-2">Sign in with a magic passwordless link</p>
-          <form onSubmit={handleSendEmail} method="POST">
-            <input
-              aria-label="Send a magic link to my email address"
-              type="text"
-              placeholder="Email address"
-              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-              onChange={({ target }) => setEmailAddress(target.value)}
-              value={emailAddress}
-            />
+            <p className="text-center text-sm mb-2">Sign in with a magic passwordless link</p>
+            {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+            {success && <p className="mb-2 text-m text-white bg-green-500 text-center rounded px-1 py-1">{success}</p>}
+            <form onSubmit={handleSendEmail} method="POST">
+              <input
+                aria-label="Send a magic link to my email address"
+                type="text"
+                placeholder="Email address"
+                className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                onChange={({ target }) => setEmailAddress(target.value)}
+                value={emailAddress}
+              />
 
-            <button
-              disabled={isInvalid}
-              type="submit"
-              className={`bg-yellow-primary text-white w-full rounded h-10 font-bold ${isInvalid && "opacity-50"}`}
-            >
-              Send Magic Sign-in link
-            </button>
-            <p className="text-center my-2 text-sm text-gray-500">or</p>
+              <button
+                disabled={isInvalid}
+                type="submit"
+                className={`bg-yellow-primary text-white w-full rounded h-10 font-bold ${isInvalid && "opacity-50"}`}
+              >
+                Send Magic Sign-in link
+              </button>
+              <p className="text-center my-2 text-sm text-gray-500">or</p>
 
-            <button
-              onClick={onSignInWithInstagramButtonClick}
-              className={"w-full rounded h-10 font-bold bg-ig-gradient"}
-            >
-              <div className="bg-white rounded-sm box-border block h-9 m-0.5">
-                <div className="text-ig-gradient block leading-9">
-                  Log in with Instagram
+              <button
+                onClick={onSignInWithInstagramButtonClick}
+                className={"w-full rounded h-10 font-bold bg-ig-gradient"}
+              >
+                <div className="bg-white rounded-sm box-border block h-9 m-0.5">
+                  <div className="text-ig-gradient block leading-9">
+                    Log in with Instagram
+                  </div>
                 </div>
-              </div>
-            </button>
-            
-            <p className="text-sm text-gray-500 text-center mt-2">Yep, for real</p>
-            
-
-          </form>
+              </button>
+            </form>
+          </div>
         </div>
-        
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,33 +4,32 @@ module.exports = {
   },
   purge: {
     enabled: process.env.PURGE_CSS === 'production' ? true : false,
+    // enabled: false,
     content: ['./src/**/*.js', './src/**/**/*.js']
   },
   theme: {
     fill: (theme) => ({
-      red: theme('colors.red.primary'),
       yellow: theme('colors.yellow.primary')
     }),
-    colors: {
-      white: '#ffffff',
-      blue: {
-        medium: '#005c98'
-      },
-      black: {
-        light: '#262626',
-        faded: '#00000059'
-      },
-      gray: {
-        base: '#616161',
-        background: '#fafafa',
-        primary: '#dbdbdb'
-      },
-      red: {
-        primary: '#ed4956'
-      },
-      yellow: {
-        primary: 'rgb(255,196,0)',
-        light: 'rgb(255, 250, 235)'
+    extend: {
+      colors: {
+        white: '#ffffff',
+        blue: {
+          medium: '#005c98'
+        },
+        black: {
+          light: '#262626',
+          faded: '#00000059'
+        },
+        gray: {
+          base: '#616161',
+          background: '#fafafa',
+          primary: '#dbdbdb'
+        },
+        yellow: {
+          primary: 'rgb(255,196,0)',
+          light: 'rgb(255, 250, 235)'
+        }
       }
     }
   },
